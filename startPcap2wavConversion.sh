16db62fccb26ae1ba0486ebe05ea58d8c651db3c
aw <awilsee@googlemail.com>
2017-09-22 13:19:28 +0200

    pcapConversion with 5 slots

--- startPcap2wavConversion.sh
+++ startPcap2wavConversion.sh
@@ -16,12 +16,18 @@ if [ ! -f "$1/$2" ]; then
     echo "$DATE file '$2' does not exist!" >> $LOGFILE
 fi
 
+FILENO=$(ls -l $WAVPATH | wc -l)
+FILENO=$(( $FILENO - 1 ))
+FILENO=$(( $FILENO / 2 ))
+FILENO=$(( $FILENO + 1 ))
+FILENO=$(( $FILENO % 5 ))
 
 echo "$DATE converting pcap to wav..." >> $LOGFILE
 #echo "$DATE /home/all/pcap2wav.sh -z \"$1/$2\" \"$WAVPATH/$2.wav\"" >> $LOGFILE
 #/home/all/pcap2wav.sh -z "$1/$2" "$WAVPATH/$2.wav"
-echo "$DATE /home/all/pcap2wav.sh -z \"$1/$2\" \"$WAVPATH/recordedCall.wav\"" >> $LOGFILE
-/home/all/pcap2wav.sh -z "$1/$2" "$WAVPATH/recordedCall.wav"
+echo "$DATE /home/all/pcap2wav.sh -z \"$1/$2\" \"$WAVPATH/recordedCall$FILENO.wav\"" >> $LOGFILE
+/home/all/pcap2wav.sh -z "$1/$2" "$WAVPATH/recordedCall$FILENO.wav"
+
 echo "$DATE script ended..." >> $LOGFILE
 
 echo >> $LOGFILE
