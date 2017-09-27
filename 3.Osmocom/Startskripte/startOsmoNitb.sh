b9c93fb02aff6293b23e6a1a69b33be1e9b3a48b
MitM <aattenbe@hm.edu>
2017-09-27 17:22:59 +0200

    Screenshots added

--- 3.Osmocom/Startskripte/startOsmoNitb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-echo "OsmoNitb wird gestartet"
-
-# Osmo-NitB starten
-cd /home/mitm/openbsc/openbsc/src/osmo-nitb
-osmo-nitb -c /home/mitm/openbsc/openbsc/src/osmo-nitb/openbsc.cfg -l /home/mitm/openbsc/openbsc/src/osmo-nitb/hlr.sqlite3 -P -C --debug=DRLL:DCC:DMM:DRR:DRSL:DNM -M /tmp/bsc_mncc
--- 3.Osmocom/Startskripte/startSipConnector.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-echo "SipConnector wird gestartet"
-
-# Osmo-NitB starten
-cd osmo-sip-connector/
-sudo osmo-sip-connector -c osmo-sip-connector.cfg
