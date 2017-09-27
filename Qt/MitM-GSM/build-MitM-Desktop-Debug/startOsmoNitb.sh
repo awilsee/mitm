64c618420a58b78cc02deb4a6f1df85f13770751
Kueken17 <alice.bollenmiller@web.de>
2017-09-27 14:34:17 +0200

    start Scripte auf allgemeinen Pfad angepasst

--- Qt/MitM-GSM/build-MitM-Desktop-Debug/startOsmoNitb.sh
+++ Qt/MitM-GSM/build-MitM-Desktop-Debug/startOsmoNitb.sh
@@ -4,5 +4,5 @@
 echo "OsmoNitb wird gestartet"
 
 # Osmo-NitB starten
-cd /home/mitm/openbsc/openbsc/src/osmo-nitb
-osmo-nitb -c /home/mitm/openbsc/openbsc/src/osmo-nitb/openbsc.cfg -l /home/mitm/openbsc/openbsc/src/osmo-nitb/hlr.sqlite3 -P -C --debug=DRLL:DCC:DMM:DRR:DRSL:DNM -M /tmp/bsc_mncc
+cd ~/openbsc/openbsc/src/osmo-nitb
+sudo osmo-nitb -c openbsc.cfg -l hlr.sqlite3 -P -C --debug=DRLL:DCC:DMM:DRR:DRSL:DNM -M /tmp/bsc_mncc
--- funktioniert/startOsmoBTS.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-echo "OsmoBTS wird gestartet"
-
-# OsmoBTS starten
-cd /home/mitm/osmo-bts/src/osmo-bts-trx
-sudo osmo-bts-trx -c osmo-bts.cfg
